f0cf32d7152080bce022aa0f64b04114d269b941
bagusprasojo <bagusprasojo@gmail.com>
2016-08-09 13:45:04 +0700

    Menambahkan Forlder SharedLib

---
+++ SharedLib/uISQLRemote.pas
@@ -0,0 +1,19 @@
+unit uISQLRemote;
+
+interface
+uses SynCommons,mORMot, uModel;
+
+
+type
+  TRemoteSQLEngine = (rseMSSQL, rseIB, rseFB, rseMYSQL, rseOleDB, rseODBC, rseOracle, rseSQlite3, rseJet);
+
+  IRemoteSQL = interface(IInvokable)
+  ['{0E34DC10-0A32-4A19-97E4-8012E06D32AA}']
+    function Execute(const aSQL: RawUTF8; aExpectResults, aExpanded: Boolean): RawJSON;
+//    function Test : Integer;
+//    function GetCOA(AID : Integer): RawJSON;
+  end;
+
+implementation
+
+end.
